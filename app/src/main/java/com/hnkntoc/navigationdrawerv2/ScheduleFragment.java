5886957a673a6146b09843f5ce70b3439c2986de
Nikita <nikita.kamyshenko@mail.ru>
2016-08-13 14:52:51 +0300

    Workaround ListCard.

--- app/src/main/java/com/hnkntoc/navigationdrawerv2/ScheduleFragment.java
+++ app/src/main/java/com/hnkntoc/navigationdrawerv2/ScheduleFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 /**
@@ -25,7 +26,24 @@ public class ScheduleFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_my, container, false);
+        LayoutInflater ltInflater = getLayoutInflater(savedInstanceState);
+        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.liner_layout);
+
+        linearLayout.addView(addNewCard(inflater,linearLayout,"Матиматика 1","Пара 1"));
+        linearLayout.addView(addNewCard(inflater,linearLayout,"Ин. яз 2","Пара 2"));
+        linearLayout.addView(addNewCard(inflater,linearLayout,"История 3","Пара 3"));
+        linearLayout.addView(addNewCard(inflater,linearLayout,"Физра 4","Пара 4"));
+
         return view;
+    }
+
+    public View addNewCard(LayoutInflater layoutInflater, ViewGroup root, String name, String description){
+        View card = layoutInflater.inflate(R.layout.custon_card_viwe, root,false);
+        TextView textView = (TextView) card.findViewById(R.id.card_name_IdNumber0);
+        textView.setText(name);
+        TextView textViewDescription = (TextView) card.findViewById(R.id.card_description_IdNumber0);
+        textViewDescription.setText(description);
+        return card;
     }
 
 }
