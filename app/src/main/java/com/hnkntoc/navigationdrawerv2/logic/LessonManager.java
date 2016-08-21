949321eb2294a145443c7d1e88d48c1ba99ab0cf
Nikita <nikita.kamyshenko@mail.ru>
2016-08-21 17:22:10 +0300

    Mini fix.

--- app/src/main/java/com/hnkntoc/navigationdrawerv2/logic/LessonManager.java
+++ app/src/main/java/com/hnkntoc/navigationdrawerv2/logic/LessonManager.java
@@ -26,6 +26,7 @@ public class LessonManager {
     private static final String TAG = LessonManager.class.getName();
     private InputStream inputStream1;
     private InputStream inputStream2;
+    private Document document;
     private DayName todayDayName;
 
     public LessonManager(ScheduleFragment fragment) {
@@ -37,16 +38,17 @@ public class LessonManager {
 
     private void updateDayName() {
         todayDayName = getDayName();
-        if (todayDayName == null){
-            Log.w(TAG,"Failed updateDayName()");
+        if (todayDayName == null) {
+            Log.w(TAG, "Failed updateDayName()");
         }
     }
 
-    public ArrayList<Lesson> getLesson()  {
+    public ArrayList<Lesson> getLesson() {
         try {
-            Document doc = ParsingHTML.transformation(
-                    ParsingHTML.parsingSchedule(inputStream1, inputStream2, "UTF-8"));
-            ExtractorSchedule extractorSchedule = new ExtractorSchedule(doc);
+            if (document == null) {
+                parsingXHTL();
+            }
+            ExtractorSchedule extractorSchedule = new ExtractorSchedule(document);
             return extractorSchedule.extractLessonWhitTime(todayDayName);
         } catch (IOException e) {
             e.printStackTrace();
@@ -54,24 +56,27 @@ public class LessonManager {
         return null;
     }
 
+    private void parsingXHTL() throws IOException {
+        Log.i(TAG, "parsingXHTL()");
+        document = ParsingHTML.transformation(
+                ParsingHTML.parsingSchedule(inputStream1, inputStream2, "UTF-8"));
+    }
+
     /**
      * Возвращает сегодняшний день.
+     *
      * @return сегодняшний день.
      */
-    private DayName getDayName(){
+    private DayName getDayName() {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
-        int day = calendar.get(Calendar.DAY_OF_WEEK);
-        switch (day){
-            //TODO для Воскресенья сейчас возврашает понедельниек так как в  DayName нет Воскресенья.
-            case 1: return DayName.MONDAY;
-            case 2: return DayName.MONDAY;
-            case 3: return DayName.TUESDAY;
-            case 4: return DayName.WEDNESDAY;
-            case 5: return DayName.THURSDAY;
-            case 6: return DayName.FRIDAY;
-            case 7: return DayName.SATURDAY;
-            default: return null;
+        int dayNumber = calendar.get(Calendar.DAY_OF_WEEK);
+        dayNumber--;
+        DayName dayName = DayName.values()[dayNumber];
+        if (dayName == null) {
+            Log.w(TAG, "getDayName return null! dayNameShort = " + dayNumber);
         }
+        Log.d(TAG, "getDayName return " + dayName);
+        return dayName;
     }
 }
