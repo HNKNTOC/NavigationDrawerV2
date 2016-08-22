d49f209010f93b36e5f79844f6ffb0ccda26ed99
Nikita <nikita.kamyshenko@mail.ru>
2016-08-22 19:19:38 +0300

    Fix bug selected tab.

--- app/src/main/java/com/hnkntoc/navigationdrawerv2/activity/MainActivity.java
+++ app/src/main/java/com/hnkntoc/navigationdrawerv2/activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.hnkntoc.navigationdrawerv2.activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -17,11 +18,32 @@ import com.parsingHTML.logic.element.DayName;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getName();
+    /**
+     * DrawerLayout который находится на  MainActivity.
+     */
     private DrawerLayout drawerLayout;
+    /**
+     * Кнопка обновленияю.
+     */
     private FloatingActionButton fab;
+    /**
+     * Toolbar который находится на  MainActivity.
+     */
     private Toolbar toolbar;
+    /**
+     * TabLayout который находится на  MainActivity.
+     */
     private TabLayout tabLayout;
+    /**
+     * ViewPager который находится на  MainActivity.
+     * для отображения DayFragment.
+     */
     private ViewPager viewPager;
+    /**
+     * Позиция TabLayout.Tab.
+     * Выделяется в зависимости от дня недели.
+     */
+    private int positionTabSelect = -1;
 
 
     @Override
@@ -38,17 +60,31 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setTitle(R.string.schedule);
 
-        setting();
+        settingFloatingActionButton(fab);
+
+        FragmentPagerAdapter adapter = createFragmentPagerAdapter();
+        viewPager.setAdapter(adapter);
+
+        drawerLayout.addDrawerListener(new MyDrawerListener());
+        tabLayout.setupWithViewPager(viewPager);
     }
 
-    private void setting() {
-        settingFloatingActionButton();
-        settingDrawerLayout();
-        settingTabLayout();
-        settingViewPager();
+    /**
+     * Настройка для FloatingActionButton.
+     */
+    private void settingFloatingActionButton(FloatingActionButton fab) {
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick FloatingActionButton.");
+            }
+        });
     }
 
-    private void settingViewPager() {
+    /**
+     * Создание и настройка FragmentPagerAdapter.
+     */
+    private FragmentPagerAdapter createFragmentPagerAdapter() {
         Adapter adapter = new Adapter(getSupportFragmentManager());
         addFragment(adapter, DayName.MONDAY);
         addFragment(adapter, DayName.TUESDAY);
@@ -57,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
         addFragment(adapter, DayName.FRIDAY);
         addFragment(adapter, DayName.SATURDAY);
         addFragment(adapter, DayName.SUNDAY);
-        viewPager.setAdapter(adapter);
+        return adapter;
     }
 
     protected void addFragment(Adapter adapter, DayName dayName) {
@@ -68,42 +104,67 @@ public class MainActivity extends AppCompatActivity {
         adapter.addFragment(fragment, dayName.getNameShort());
     }
 
-    private void settingTabLayout() {
-        tabLayout.setupWithViewPager(viewPager);
-    }
-
-    private void settingFloatingActionButton() {
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, "onClick FloatingActionButton.");
-            }
-        });
-    }
-
-
-    private void settingDrawerLayout() {
-        drawerLayout.addDrawerListener(new MyDrawerListener());
-    }
 
     @Override
     protected void onStart() {
         super.onStart();
+        selectTabToday();
+    }
+
+    /**
+     * Выделить Tab с сегодняшним днём.
+     */
+    private void selectTabToday() {
         int ordinal = LessonManager.getDayName().ordinal();
-        Log.i(TAG, "Set CurrentItem = " + ordinal);
-        viewPager.setCurrentItem(ordinal);
-        TabLayout.Tab tabAt = tabLayout.getTabAt(ordinal);
-        if (tabAt != null) {
-            tabAt.setIcon(R.drawable.error);
+        Log.i(TAG, "selectTabToday() ordinal = " + ordinal + " positionTabSelect = " + positionTabSelect);
+
+        if (positionTabSelect == -1) {
+            Log.d(TAG, "positionTabSelect == -1.");
+            selectTab(ordinal);
+            return;
+        }
+
+        if (positionTabSelect == ordinal) {
+            Log.d(TAG, "Tab saved position.");
         } else {
-            Log.w(TAG, "Failed Tab setIcon  Tab = null");
+            Log.d(TAG, "Tab new position.");
+            resetSelectTab(positionTabSelect);
+            selectTab(ordinal);
         }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    /**
+     * Выбрать Tab.
+     *
+     * @param position позиция Tab.
+     */
+    private void selectTab(int position) {
+        TabLayout.Tab tab = tabLayout.getTabAt(position);
+        if (tab != null) {
+            tab.setIcon(R.drawable.error);
+            viewPager.setCurrentItem(position);
+            Log.d(TAG, "Select tab = " + tab.getText());
+            positionTabSelect = position;
+        } else {
+            Log.w(TAG, "Select tab == null!");
+        }
     }
+
+    /**
+     * Убрать вобор с Tab.
+     *
+     * @param position позиция Tab.
+     */
+    private void resetSelectTab(int position) {
+        TabLayout.Tab tabAt = tabLayout.getTabAt(position);
+        if (tabAt != null) {
+            tabAt.setIcon(null);
+            Log.d(TAG, "resetSelectTab() to " + tabAt.getText());
+        } else {
+            Log.w(TAG, "Failed resetSelectTab() tabAt == null. positionTabSelect = " + this.positionTabSelect);
+        }
+    }
+
 
     /**
      * Слушатель для открытия и закрытия меню.
