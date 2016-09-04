f36e62bd2695937944c88f1db0406beb8dc8ae94
Nikita <nikita.kamyshenko@mail.ru>
2016-09-04 15:06:59 +0300

    Add DescriptionActivity.

---
+++ app/src/main/java/com/hnkntoc/navigationdrawerv2/activity/DescriptionActivity.java
@@ -0,0 +1,52 @@
+package com.hnkntoc.navigationdrawerv2.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.hnkntoc.navigationdrawerv2.R;
+import com.hnkntoc.navigationdrawerv2.logic.CardViewFactory;
+import com.parsingHTML.logic.extractor.xml.Lesson;
+
+/**
+ * Отображает подробное описание урока.
+ */
+public class DescriptionActivity extends AppCompatActivity {
+    private static final String TAG = DescriptionActivity.class.getName();
+
+    public static final String KEY_LESSON = "KeyLesson";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate()");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_description);
+
+        Lesson lesson = (Lesson) getIntent().getSerializableExtra(KEY_LESSON);
+
+        Log.d(TAG, "Lesson = " + lesson);
+
+        TextView textViewLessonName = (TextView) findViewById(R.id.desc_name);
+        textViewLessonName.setText(lesson.getName());
+
+        TextView textViewTime1 = (TextView) findViewById(R.id.desc_time1);
+        textViewTime1.setText(lesson.getTime1());
+
+        TextView textViewTime2 = (TextView) findViewById(R.id.desc_time2);
+        textViewTime2.setText(lesson.getTime2());
+
+        TextView textViewLessonDescription = (TextView) findViewById(R.id.desc_description);
+        textViewLessonDescription.setText(lesson.getDescription());
+
+        ImageView imageViewLessonNumber = (ImageView) findViewById(R.id.desc_image_number);
+        imageViewLessonNumber.setImageResource(CardViewFactory.getImageLessonNumber(lesson.getNumber()));
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy()");
+        super.onDestroy();
+    }
+}
